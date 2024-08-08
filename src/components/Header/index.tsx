df761e113816982c80fcc3653c11e4c1c2da86e3
Carolina Limeres <carolinalimeres@outlook.com>
2024-08-08 14:37:51 +0100

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,97 @@
 import { HeaderContainer } from "./styles";
+import { LinkedinLogo } from "phosphor-react";
+import { MagnifyingGlass } from "phosphor-react";
+import { HouseLine } from "phosphor-react";
+import { Users } from "phosphor-react";
+import { Bag } from "phosphor-react";
+import { ChatCircleDots } from "phosphor-react";
+import { Bell } from "phosphor-react";
+import { CaretDown } from "phosphor-react";
+import { Gradient } from "phosphor-react";
+import profileImage from "../../../public/profile.jpg";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <p>TESTE DO HEADER</p>
+      <div className="logo-search-container">
+        <a href="" className="link-logo-container">
+          <LinkedinLogo
+            size={45}
+            weight="fill"
+            className="linkedin-logo"
+          ></LinkedinLogo>
+        </a>
+        <div className="search-container">
+          <MagnifyingGlass
+            size={16}
+            weight="bold"
+            className="search-icon"
+          ></MagnifyingGlass>
+
+          <input type="text" placeholder="Pesquisar" className="input-search" />
+        </div>
+      </div>
+
+      <div className="options-container">
+        <div className="icon-text-container">
+          <a href="" className="link-container">
+            <HouseLine size={24} weight="fill" className="icon"></HouseLine>
+            <p className="text-container">Início</p>
+          </a>
+        </div>
+        <div className="icon-text-container">
+          <a href="" className="link-container">
+            <Users size={24} weight="fill" className="icon"></Users>
+            <p className="text-container">Minha rede</p>
+          </a>
+        </div>
+        <div className="icon-text-container">
+          <a href="" className="link-container">
+            <Bag size={24} weight="fill" className="icon"></Bag>
+            <p className="text-container">Vagas</p>
+          </a>
+        </div>
+        <div className="icon-text-container">
+          <a href="" className="link-container">
+            <ChatCircleDots
+              size={24}
+              weight="fill"
+              className="icon"
+            ></ChatCircleDots>
+            <p className="text-container">Mensagens</p>
+          </a>
+        </div>
+        <div className="icon-text-container">
+          <a href="" className="link-container">
+            <Bell size={24} weight="fill" className="icon"></Bell>
+            <p className="text-container">Notificações</p>
+          </a>
+        </div>
+
+        <div className="photo-text-container">
+          <img src={profileImage} alt="" className="profile-image" />
+          <div className="text-arrow-container">
+            <p className="me-text">Eu</p>
+            <CaretDown weight="fill"></CaretDown>
+          </div>
+        </div>
+      </div>
+      <div className="business-premium-container">
+        <div className="business-container">
+          <Gradient
+            size={28}
+            weight="fill"
+            className="gradient-icon"
+          ></Gradient>
+          <div className="text-arrow-container">
+            <p className="business-text">Para negócios</p>
+            <CaretDown weight="fill"></CaretDown>
+          </div>
+        </div>
+        <div className="premium-container">
+          Avance na sua carreira com Premium
+        </div>
+      </div>
     </HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,9 +2,157 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background-color: #ffffff;
-  /* acho que esse fixed só pode ser liberado quando tiver barra de rolagem no site: */
-  /* position: fixed; */
-  height: 3.541rem;
+  position: fixed;
+
+  height: 3.54rem;
   width: 100vw;
   border-bottom: 1px solid rgba(140, 140, 140, 0.2);
+
+  display: flex;
+  align-items: center;
+
+  justify-content: space-evenly;
+
+  .options-container {
+    display: flex;
+    gap: 1.75rem;
+  }
+
+  .logo-search-container {
+    display: flex;
+    align-items: center;
+    gap: 0.125rem;
+  }
+
+  .link-logo-container {
+    display: flex;
+  }
+
+  .linkedin-logo {
+    color: #0a66c2;
+    cursor: pointer;
+  }
+
+  .search-container {
+    display: flex;
+    align-items: center;
+    gap: 0.375rem;
+
+    position: relative;
+    width: 17.5rem;
+  }
+
+  .search-icon {
+    color: rgb(0, 0, 0, 0.9);
+
+    position: absolute; /* pro ícone ficar dentro do input (lembrar de colocar relative no pai) */
+    left: 1rem;
+    pointer-events: none; /* com essa propriedade, se eu clico no ícone
+    da lupa libera o input tb (além do clique na caixa normal). Sem isso, ao clicar na lupa
+    nao libera o input */
+  }
+
+  .input-search {
+    border: none;
+    box-shadow: none;
+
+    width: 100%;
+    font-weight: 400;
+    font-size: 0.875rem;
+    height: 2.125rem;
+
+    background-color: #edf3f8;
+    color: rgb(0, 0, 0, 0.9);
+    border-radius: 0.4rem;
+    padding: 0 2.5rem;
+  }
+
+  .link-container {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: rgb(0, 0, 0, 0.6);
+  }
+
+  .link-container:hover {
+    color: rgb(0, 0, 0, 0.9);
+  }
+
+  .text-arrow-container:hover {
+    color: rgb(0, 0, 0, 0.9);
+  }
+
+  .business-container:hover {
+    color: rgb(0, 0, 0, 0.9);
+  }
+
+  .premium-container:hover {
+    color: rgb(0, 0, 0, 0.9);
+  }
+
+  .text-container {
+    font-size: 0.75rem;
+    line-height: 1.33333;
+  }
+
+  .photo-text-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .profile-image {
+    border-radius: 50%;
+    border: none;
+    height: 24px;
+    overflow: hidden;
+    transform: scale(1);
+    transition: transform 0.2s ease-in-out;
+    width: 24px;
+  }
+
+  .text-arrow-container {
+    display: flex;
+    color: rgb(0, 0, 0, 0.6);
+    gap: 0.125rem;
+  }
+
+  .business-premium-container {
+    display: flex;
+    align-items: center;
+    gap: 0.875rem;
+
+    border-left: 1px solid rgba(140, 140, 140, 0.2);
+    height: 100%;
+    padding-left: 1.2rem;
+  }
+
+  .business-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    cursor: pointer;
+  }
+
+  .gradient-icon {
+    color: rgb(0, 0, 0, 0.6);
+  }
+
+  .business-text,
+  .premium-container,
+  .me-text {
+    font-size: 0.75rem;
+  }
+
+  .premium-container {
+    max-width: 8.75rem;
+    text-align: center;
+    cursor: pointer;
+    text-decoration: underline;
+    color: #915907;
+    line-height: 1.5;
+  }
 `;
